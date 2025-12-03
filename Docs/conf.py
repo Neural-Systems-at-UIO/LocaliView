475fb8b1a7ab229f508c5034b9662704e9270b8a
Maja <46992863+Majpuc@users.noreply.github.com>
2025-12-03 10:02:12 +0100

    Update conf.py

--- Docs/conf.py
+++ Docs/conf.py
@@ -27,7 +27,7 @@ author = 'NeSys'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = []
+extensions = ['sphinx_rtd_theme']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -35,7 +35,7 @@ templates_path = ['_templates']
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['sphinx_rtd_theme']
+exclude_patterns = []
 
 
 
@@ -53,11 +53,13 @@ html_theme_options = {
     'navigation_depth': 4,
     'includehidden': True,
     'titles_only': False
-    'logo_only': True,
-    'display_version': False}
+    }
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 html_logo = "Logos_LocaliView.png"
-
+html_theme_options = {
+    'logo_only': True,
+    'display_version': False
+ }
